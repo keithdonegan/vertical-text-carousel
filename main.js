85454d987a0b576b116886c63647715d5dc4c73a
Keith Donegan <keith.donegan@gmail.com>
2025-07-10 22:36:33 +0100

    latest

--- main.js
+++ main.js
@@ -2,6 +2,18 @@ fehVerticalCarousel({
     itemsToShow: 6,
     responsive: [
         {
+            breakpoint: 900, // viewport height > 900px
+            settings: {
+                itemsToShow: 6,
+            }
+        },
+        {
+            breakpoint: 500, // viewport height > 1200px
+            settings: {
+                itemsToShow: 4,
+            }
+        },
+        {
             breakpoint: 0, // viewport height < 500px
             settings: {
                 itemsToShow: 2,
